24775f08f950b1af6325d079ac98740d3bfe2824
Balazs Nadasdi <yitsushi@gmail.com>
2014-10-25 00:51:08 +0200

    add package.json

---
+++ test/index.js
@@ -0,0 +1,15 @@
+var Mocha = require('mocha');
+
+var mocha = new Mocha({ timeout: 5000 });
+mocha.ui('tdd');
+
+files = [
+];
+
+for(var i = 0, _l = files.length; i < _l; i++) {
+    mocha.addFile(files[i]);
+}
+
+var runner = mocha.run(function(code) {
+    process.exit(code);
+});
